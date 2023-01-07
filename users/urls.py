e30504ddb8de093f39c204e00a84893ece498799
Bereket Godebo <bereket.godebo@outlook.com>
2023-01-07 20:25:40 +0300

    completed module 7

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	path('sign-up/', views.SignUpView.as_view(), name="sign-up"),
 	path('sign-in/', views.SignInView.as_view(), name="sign-in"),
 	path('sign-out/', views.sign_out, name="sign-out"),
-	path('account/', views.AccountView.as_view(), name="account"),
-    path('info/', views.UserInfoView.as_view(), name="info"),
+	path('account/', views.AccountView, name="account"),
+    path('info/', views.UserInfoView, name="info"),
 	]
